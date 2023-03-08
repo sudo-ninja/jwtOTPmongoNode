2b49c4dc1edc759137b431ebd082abdd8e39389e
sudo-ninja <jenixjain@gmail.com>
2023-03-08 15:23:25 +0530

    implement login with JWtoken  functionality

--- src/app.js
+++ src/app.js
@@ -11,11 +11,18 @@ const routes = require("./routes");
 
 // create server app
 const app = express();
-// initiaise server app
+// initialise server app
+
 app.use(cors());
 app.use(bodyParser());
 
 app.use("/api/v1",routes);
+
+//for new user signup use below string in postman
+// post : localhost:5000/api/v1/user/signup
+
+//for login use below string in postman
+//post : localhost:5000/api/v1/user/
 
 //expose this module to main index.js in src file
 module.exports = app;
--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -1,6 +1,29 @@
 const express = require ("express");
 const router = express.Router();
-const {createNewUser} = require("./controller")
+const {createNewUser,authenticateUser} = require("./controller")
+
+//Sign in
+
+router.post("/",async(req,res)=>{
+    try {
+        let {email,password} = req.body;
+        email=email.trim();
+        password=password.trim();  
+
+        //check condition
+        if(!(email&&password)){
+            throw Error("Empty credentials!");
+        }
+        //authentication checked
+        const authenticatedUser = await authenticateUser({
+            email,password });
+        res.status(200).json(authenticatedUser);
+
+    } catch (error) {
+        res.status(400).send(error.message);
+        
+    }
+});
 
 // signup
 router.post("/signup",async(req,res)=>{
